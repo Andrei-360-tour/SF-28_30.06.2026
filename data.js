1729e8f68e0f140cb10a8bf50dce5e7fbb76cc32
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-07-03 09:24:05 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overview-1",
+      "name": "OVERVIEW 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0295262460667729,
+        "pitch": 0.17173462636492864,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19141143899469704,
+          "pitch": 0.1251862264349164,
+          "rotation": 0,
+          "target": "1-overview-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-overview-2",
+      "name": "OVERVIEW 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2112570909814,
+        "pitch": 0.5509997983405643,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11452057159643303,
+          "pitch": 0.21286794928090735,
+          "rotation": 0,
+          "target": "0-overview-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SF-28_30.06.2026",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
